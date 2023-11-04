97d8783c66d9595029b2c72674555d4fb0ca6982
contehboy <133304258+contehboy@users.noreply.github.com>
2023-11-04 10:57:48 -0500

    added final project in portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import walletwizard from "../../assets/walletwizard.png";
 import travelplanner from "../../assets/travelplanner.png";
 import texteditor from "../../assets/texteditor.png";
+import workout from "../../assets/workoutapp.png";
 
 function Portfolio() {
   return (
@@ -75,6 +76,34 @@ function Portfolio() {
             <p>
               A text editor to save your notes that runs in the browser and also
               function offline
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="project">
+        <div class="project-info">
+          <div class="project-img">
+            <a href="https://still-fortress-13697-122e1b6f4e62.herokuapp.com/">
+              {" "}
+              <img
+                src={workout}
+                class="project-img2"
+                style={{ width: "100%" }}
+                alt="wallet wizard"
+              />
+            </a>
+          </div>
+          <div class="project-text">
+            <h4>Workout app</h4>
+            <p>
+              This application can be used to track the progress of you
+              workouts. It alows users to create custom workouts with exercises
+              and sets. The user can input reps and weights to the sets and log
+              the workouts to the database. The progress page shows the data
+              from these logged workouts that you can view based on exercise.
+              Each set outputs a calculated daily max rep value that can be
+              charted over time. This is usefull to view your strength progress
+              as you train.
             </p>
           </div>
         </div>
